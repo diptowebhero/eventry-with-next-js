6da94728b6cac76a6d782f958855b2b65154650a
Dipto Das <diptodas1672@gmail.com>
2025-06-30 13:42:16 +0600

    add loading ui

--- components/landing/EventCard.jsx
+++ components/landing/EventCard.jsx
@@ -6,24 +6,22 @@ const EventCard = ({ event }) => {
   return (
     <div className='overflow-hidden rounded-md bg-[#242526]'>
       <Image
-        src='/google-io-2023-1.png'
-        alt='Event 1'
-        className='w-full'
+        src={event.imageUrl}
+        alt={`Image for ${event.name}`}
+        className='w-full h-[300px] object-cover'
         width={500}
         height={500}
       />
 
       <div className='p-3'>
         <Link href={`/details/${event.id}`} className='font-bold text-lg'>
-          Google I/O Extended
+          {event.name}
         </Link>
-        <p className='text-[#9C9C9C] text-sm mt-1'>
-          Rangpur, Dhaka, Bangladesh, Rangpur, Bangladesh
-        </p>
-        <div className='text-[#737373] text-sm mt-1'>
-          <span>1k Interested</span>
+        <p className='text-[#9C9C9C] text-sm mt-1'>{event.location}</p>
+        <div className='text-[#737373] text-sm mt-1 space-x-2'>
+          <span>{event.interested_ids?.length} Interested</span>
           <span>|</span>
-          <span>40K Going</span>
+          <span>{event.going_ids?.length} Going</span>
         </div>
         <ActionButtons />
       </div>
